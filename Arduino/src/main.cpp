561f43d3d5aaf86892e50d06a42a3142d4103df3
DakshK26 <khannad24@gmail.com>
2025-02-16 00:25:22 -0500

    Added Arduino

---
+++ Arduino/src/main.cpp
@@ -0,0 +1,60 @@
+#include <Arduino.h>
+#include "dht.h"
+
+#define dht_apin A0  // Analog Pin where temperature and humidity sensor is connected
+#define moistureSensorPin A1  // Analog Pin where soil moisture sensor is connected
+
+// Initialize DHT device
+dht DHT;
+
+// Moisture data
+int moistureValue = 0;
+
+// Timing variables
+unsigned long lastCheck = 0;
+const unsigned long timeInterval = 2000;  // 2 seconds
+
+/*
+Setup
+*/
+void setup() {
+  delay(500);
+  Serial.begin(9600);      // Initialize Serial Monitor (USB)
+  
+  delay(1000);
+  Serial.println("Plant Data\n\n");
+}
+
+/*
+Functions
+*/
+
+// Function to measure temperature and humidity
+void tempratureAndHumidity() {
+  DHT.read11(dht_apin); // Read temperature and humidity
+}
+
+// Function to measure soil moisture
+void soilMoistureSensor() {
+  moistureValue = analogRead(moistureSensorPin); // Read the sensor value
+}
+
+void printAll(){
+  // Format data string
+  String data = "Humidity: " + String(DHT.humidity) + "%, Temperature: " + String(DHT.temperature) + "C" + "\nSoil Moisture Value: " + String(moistureValue);
+  Serial.println(data);
+}
+
+/*
+Loop
+*/
+void loop() {
+  unsigned long currentMillis = millis();
+  // Check soil moisture every 2 seconds
+  if (currentMillis - lastCheck >= timeInterval) {
+    lastCheck = currentMillis;
+    tempratureAndHumidity();
+    soilMoistureSensor();
+    printAll();
+  }
+}
